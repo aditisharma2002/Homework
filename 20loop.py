3393e48c610405727f055647a28e325fe6c5aa0d
aditisharma2002 <122394069+aditisharma2002@users.noreply.github.com>
2023-01-31 22:17:43 -0800

    Update 20loop.py

--- 20loop.py
+++ 20loop.py
@@ -1,10 +1,14 @@
 # 20loop.py
 
+# Aditi Sharma
+# January 31 2023
 # Print out the numbers from 5 to less than 50 in steps of 7
 
 # Hint: use the 3 argument form of range()
 
 # Your code goes here
+for i in range (5, 50, 7):
+print(i)
 
 """
 python3 20loop.py
