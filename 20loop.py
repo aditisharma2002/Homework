7740cf8af68604d012b9599d14bd505d67917169
aditisharma2002 <aditishar2002@gmail.com>
2023-01-31 22:54:52 -0800

    new

--- 20loop.py
+++ 20loop.py
@@ -1,24 +1,9 @@
 # 20loop.py
 
-# Aditi Sharma
-# January 31 2023
-# MCB 183-2023
-
 # Print out the numbers from 5 to less than 50 in steps of 7
 
 # Hint: use the 3 argument form of range()
 
 # Your code goes here
 for i in range (5, 50, 7):
-print(i)
-
-"""
-python3 20loop.py
-5
-12
-19
-26
-33
-40
-47
-"""
+		print (i)
