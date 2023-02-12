7bcc3c64b67b7b625eaa3eda2ae1c70ac7ba2dd6
aditisharma2002 <aditishar2002@gmail.com>
2023-02-12 14:43:11 -0800

    init

--- 21codons.py
+++ 21codons.py
@@ -6,6 +6,9 @@
 
 dna = 'ATAGCGAATATCTCTCATGAGAGGGAA'
 
+for a in range(0, len(dna), 3): # here we are saying for the variable in the range of 0 to the length of the dna using step of 3.
+	codon = dna[a: a + 3]
+	print(codon)  
 
 """
 python3 21codons.py
