45dd9291bd2d34ce34e1d6f6893273e8f32e5e23
aditisharma2002 <aditishar2002@gmail.com>
2023-02-12 15:23:01 -0800

    init

--- 23fizzbuzz.py
+++ 23fizzbuzz.py
@@ -6,7 +6,15 @@
 # For numbers which are multiples of both 3 and 5 print “FizzBuzz”
 
 # Note: this is a common interview question
-
+for i in range(1,101): # we use 101 because we are starting from 1
+	if i%5 == 0 and i%3 == 0:
+		print("FizzBuzz")
+	elif i%3 == 0:
+		print("Fizz")
+	elif i%5 == 0:
+		print("Buzz")
+	else:
+		print(i)
 
 """
 python3 23fizzbuzz.py
