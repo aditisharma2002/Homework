0a5df46ff8bf22fdb43078e502c10193175a5b62
aditisharma2002 <aditishar2002@gmail.com>
2023-02-12 16:11:40 -0800

    init

--- 24gc.py
+++ 24gc.py
@@ -5,7 +5,50 @@
 
 dna = 'ACAGAGCCAGCAGATATACAGCAGATACTAT'
 
+#first we are going to assign values to the variables so that we can store other values in them later on
+g = 0
+c = 0
 
+#now we have to make it so that every time there is a G or C, 1 is added.
+for BP in dna:
+	if BP == 'G':
+		g = g + 1
+	elif BP == 'C':
+		c = c + 1
+	gc_count = c + g
+# we want it to be like an average and that is why we take the value that we get above and then divide by the total in the dna.
+print(f'{gc_count/len(dna):.2f}')
+#now I got 0.42 as the answer
+
+
+#first we are going to assign values to the variables so that we can store other values in them later on
+#g = 0
+#c = 0
+
+#now we have to make it so that every time there is a G or C, 1 is added.
+#for BP in dna:
+#	if BP == 'G':
+#		g = g + 1
+#	elif BP == 'C':
+#		c = c + 1
+#print(BP)
+# did the same thing and only printed T 
+
+
+# first we are going to assign values to the variables so that we can store other values in them later on
+#G = 0
+#C = 0
+
+#now we have to make it so that every time there is a G or C, 1 is added.
+#for BP in dna:
+	#if BP == 'G':
+	#	G = G + 1
+	#elif BP == 'C':
+	#	C = C + 1
+#print(BP)
+# when I did this, I got T which is definitely not what we want.
+# maybe the computer is getting confused because I am using G and C as variable but they are also in the sequence. 
+# changed G and C to g and c
 """
 python3 24gc.py
 0.42
--- 25atseq.py
+++ 25atseq.py
@@ -8,7 +8,23 @@
 
 # Note: set random.seed() if you want repeatable random numbers
 
-
+import random #in question it says that we need to import a random sequence
+seq_len = 30
+seq = ''
+fraction_AT = 0.6
+at = 0
+# here we are saying that if the basepair in the sequence of nucleotides is A or T, then add 1
+# then we say in the en
+for NT in range(seq_len):
+	for BP in seq:
+		if BP =='A' or BP == 'T':
+			at = at + 1
+	r = random.random() # use this because we want random float numbers 
+	if r <= fraction_AT: seq = seq + random.choice('AT')
+	else:
+		seq = seq + random.choice('CG')
+print(len(seq), at/len(seq), seq)
+# when i did this, I got the wrong values for the decimal and the sequence after it but I think this is because a random sequence is picked every time.
 """
 python3 25atseq.py
 30 0.6666666666666666 ATTACCGTAATCTACTATTAAGTCACAACC
