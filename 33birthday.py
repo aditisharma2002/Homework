0390d4ff94952ebfa150ddb930c8371ab2205d20
aditisharma2002 <aditishar2002@gmail.com>
2023-02-19 12:36:31 -0800

    init

--- 33birthday.py
+++ 33birthday.py
@@ -11,6 +11,48 @@
 # Variation: try making the birthdays a list
 
 
+
+import random
+import sys
+
+num_days = int(sys.argv[1])
+
+num_people = int(sys.argv[2])
+
+print(num_days, num_people)
+
+trials = 1000
+shared = 0
+
+for trial in range(trails):
+	calendar = [0] * num_days
+	for i in range(num_people):
+		birthday = random.randint(0, num_days - 1)
+		if calendar[birthday] == 1:
+			shared += 1
+			break
+		calendar[birthday] += 1
+print(shared/trials)
+
+
+
+
+
+
+
+
+
+# This is WRONG
+#for trial in range(trials):
+#	birthdays = []
+#	for i in range(num_people):
+#		birthday = random.randint(0, num_days - 1)
+#		if birthday in birthdays:
+#			shared += 1
+#			break
+#		birthday.append(birthday)
+#print(shared/trial)
+
 """
 python3 33birthday.py 365 23
 0.571
