792c9b1b5ed6db97d8641958ebc434764043d30a
aditisharma2002 <aditishar2002@gmail.com>
2023-02-25 18:58:32 -0800

    init

--- 42dust.py
+++ 42dust.py
@@ -12,6 +12,41 @@
 
 # Note: don't worry about "centering" the entropy on the window (yet)
 
+import sys
+import math
+import mcb185
+
+file = sys.argc[1]
+window = int(sys.argv[2])
+entropy_threshold = float(sys.argv[3])
+
+#first step would be to make the entropy calculator 
+def entropy(probability):
+	assert(math.isclose(1.0, sum(probability)))
+	h = 0
+	for prob in probability:
+		if prob != 0: h -= prob * math.log2(p)
+	return h
+
+#so we made just a regular entropy calculator but need to make one that does it for dna sequences
+def seq_entropy(seq):
+	A = seq.count('A')/len(seq)
+	T = seq.count('T')/len(seq)
+	G = seq.count('G')/len(seq)
+	C = seq.count('C')/len(seq)
+	return entropy([A, T, G, C])
+	
+for thing, seq in MCB185.read.fasta(sys.argv[1]):
+	seqX = list(seq)
+	for i in range(len(seq) - window + 1):
+		w = seq[i: i + w]
+		e = seq_entropy(w)	
+	seq =''.join(seqX)
+	print(seq)
+
+
+
+
 
 """
 python3 42dust.py ~/DATA/E.coli/GCF_000005845.2_ASM584v2_genomic.fna.gz 11 1.4
